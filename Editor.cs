fd582913b5778fa502d7c989d723f7c11fa709d0
Carlos Barbosa <77971569+carloslk18@users.noreply.github.com>
2025-11-27 04:00:15 -0300

    Update Editor.cs

--- Editor.cs
+++ Editor.cs
@@ -32,3 +32,93 @@ public class Editor{
     }
 }
 }
+namespace HtmlEditor{
+
+public class Editor{
+
+    public static void Show(){
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Clear();
+        Console.WriteLine("Editor Mode");
+        Console.WriteLine("-----------");
+        Start();
+    }
+
+    public static void Start(){
+        var file = new StringBuilder();
+
+        do{
+            file.Append(Console.ReadLine());
+            file.Append(Environment.NewLine);
+        }
+        while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+        Console.WriteLine("-----------");
+        Console.WriteLine("Do you want to save the file?");
+
+        Viewer.Show(file.ToString());
+
+    }
+}
+    namespace HtmlEditor{
+
+public class Editor{
+
+    public static void Show(){
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Clear();
+        Console.WriteLine("Editor Mode");
+        Console.WriteLine("-----------");
+        Start();
+    }
+
+    public static void Start(){
+        var file = new StringBuilder();
+
+        do{
+            file.Append(Console.ReadLine());
+            file.Append(Environment.NewLine);
+        }
+        while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+        Console.WriteLine("-----------");
+        Console.WriteLine("Do you want to save the file?");
+
+        Viewer.Show(file.ToString());
+
+    }
+}
+        namespace HtmlEditor{
+
+public class Editor{
+
+    public static void Show(){
+        Console.Clear();
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.ForegroundColor = ConsoleColor.Black;
+        Console.Clear();
+        Console.WriteLine("Editor Mode");
+        Console.WriteLine("-----------");
+        Start();
+    }
+
+    public static void Start(){
+        var file = new StringBuilder();
+
+        do{
+            file.Append(Console.ReadLine());
+            file.Append(Environment.NewLine);
+        }
+        while(Console.ReadKey().Key != ConsoleKey.Escape);
+
+        Console.WriteLine("-----------");
+        Console.WriteLine("Do you want to save the file?");
+
+        Viewer.Show(file.ToString());
+
+    }
+}
