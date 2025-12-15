be7277b4f2bca304a1b339c8800d35aceeac0e17
Carlos Barbosa <77971569+carloslk18@users.noreply.github.com>
2025-12-15 12:49:29 -0300

    Fix foreground color setting in Show method

--- Editor.cs
+++ Editor.cs
@@ -100,4 +100,3 @@ public class Editor{
         Console.Clear();
         Console.BackgroundColor = ConsoleColor.White;
         Console.ForegroundColor = ConsoleColor.Black;
-        Console.Clear();
